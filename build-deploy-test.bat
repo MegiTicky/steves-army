56b485b61977f304cd3f846fb41258b9026972ef
MegiRicky <lauyathong3518@gmail.com>
2026-08-31 22:10:25 +0800

    Add build prompt to test deploy script

--- build-deploy-test.bat
+++ build-deploy-test.bat
@@ -17,6 +17,19 @@ if not exist "%TEST_MODS%" (
 )
 
 pushd "%PROJECT_DIR%"
+call :select_artifact
+if not defined ARTIFACT (
+    echo No existing deployable JAR was found in "%PROJECT_DIR%build\libs".
+    echo A new build is required.
+) else (
+    echo Current selected deploy JAR: "%ARTIFACT%"
+)
+
+choice /c YN /n /m "Build a new JAR before deploying? [Y/N] "
+if errorlevel 2 goto :after_build
+
+echo.
+echo Building mod JAR...
 call gradlew.bat build
 if errorlevel 1 (
     echo ERROR: Gradle build failed. The existing Test instance JAR was not changed.
@@ -24,12 +37,14 @@ if errorlevel 1 (
     exit /b 1
 )
 
-for /f "delims=" %%F in ('dir /b /a-d "build\libs\*.jar" 2^>nul') do set "ARTIFACT=%%F"
+:after_build
+call :select_artifact
 if not defined ARTIFACT (
     echo ERROR: No JAR was produced in "%PROJECT_DIR%build\libs".
     popd
     exit /b 1
 )
+echo Selected deploy JAR: "%ARTIFACT%"
 
 set "SOURCE=%PROJECT_DIR%build\libs\%ARTIFACT%"
 set "DESTINATION=%TEST_MODS%\%ARTIFACT%"
@@ -47,3 +62,8 @@ for %%F in ("%DESTINATION%") do echo Size: %%~zF bytes
 
 popd
 exit /b 0
+
+:select_artifact
+set "ARTIFACT="
+for /f "delims=" %%F in ('dir /b /a-d /o-d "build\libs\*.jar" 2^>nul') do if not defined ARTIFACT set "ARTIFACT=%%F"
+exit /b
